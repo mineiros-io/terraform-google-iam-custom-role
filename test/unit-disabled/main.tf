0a7097ff1e9d3abd0f3aff3eb3713a717425dd62
Bence Wax <bence.wax@mineiros.io>
2022-09-27 12:25:47 +0200

    chore: module template update

--- test/unit-disabled/main.tf
+++ test/unit-disabled/main.tf
@@ -4,32 +4,6 @@
 # The purpose is to verify no resources are created when the module is disabled.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-variable "gcp_region" {
-  type        = string
-  description = "(Required) The gcp region in which all resources will be created."
-}
-
-variable "gcp_project" {
-  type        = string
-  description = "(Required) The ID of the project in which the resource belongs."
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/google"
-      # always test with exact version to catch unsupported blocks/arguments early
-      # this should match the minimal version in versions.tf
-      version = "4.8.0"
-    }
-  }
-}
-
-provider "google" {
-  region  = var.gcp_region
-  project = var.gcp_project
-}
-
 # DO NOT RENAME MODULE NAME
 module "test" {
   source = "../.."
--- test/unit-minimal/main.tf
+++ test/unit-minimal/main.tf
@@ -4,41 +4,11 @@
 # The purpose is to test all defaults for optional arguments and just provide the required arguments.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-variable "gcp_region" {
-  type        = string
-  description = "(Required) The gcp region in which all resources will be created."
-}
-
-variable "gcp_project" {
-  type        = string
-  description = "(Required) The ID of the project in which the resource belongs."
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/google"
-      # always test with exact version to catch unsupported blocks/arguments early
-      # this should match the minimal version in versions.tf
-      version = "4.8.0"
-    }
-  }
-}
-
-provider "google" {
-  region  = var.gcp_region
-  project = var.gcp_project
-}
-
-
 # DO NOT RENAME MODULE NAME
 module "test" {
   source = "../.."
 
   role_id     = "myCustomRole"
   title       = "My Custom Role"
-  description = "A description"
   permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
-
-  org_id = "1234566"
 }
